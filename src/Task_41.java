a55add1fe69c2114fc5c4c97baf4e36d9c14c8b5
AlexeySirant <lexsirant@gmail.com>
2019-05-01 18:33:18 +0300

    41

---
+++ src/Task_41.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task_41 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter Integer:");
+        int grade = Integer.parseInt(br.readLine());
+//        String gradeLevel;
+        if(grade >= 1 && grade <= 3 ) {
+            System.out.println("The pupil's level is Initial");
+        } else if (grade >= 4 && grade <= 6 ) {
+            System.out.println("The pupil's level is Average");
+        } else if (grade >= 7 && grade <= 9 ){
+            System.out.println("The pupil's level is Sufficient");
+        } else if (grade >= 10 && grade <= 12 ) {
+            System.out.println("The pupil's level is High");
+        }
+        }
+    }
+
