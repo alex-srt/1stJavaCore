e95bca5b8a5c10acd50d4c4f2a9407ce057616de
lx5 <lexsirant@gmail.com>
2019-05-04 11:58:15 +0300

    42_done

--- src/Task_42.java
+++ src/Task_42.java
@@ -1,22 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Task_42 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter Integer:");
-        int monthNumber = Integer.parseInt(br.readLine());
-        System.out.println(monthNumber);
-
-        System.out.print("Введіть будь-яке ціле число від 1 до 10: ");
+    public static void main(String[] args)  {
+        System.out.print("Enter month number 1 - 12: ");
         Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        System.out.println ("Ви ввели число " + number);
+        if (scan.hasNextInt()) {
+            int myMonth = scan.nextInt();
+            System.out.println("Month # is " + myMonth);
 
-
-
+            if(myMonth >= 3 && myMonth <= 5 ) {
+                System.out.println("The season is Spring");
+            } else if (myMonth  >= 6 && myMonth <= 8 ) {
+                System.out.println("The season is Summer");
+            } else if (myMonth >= 9 && myMonth <= 11 ){
+                System.out.println("The season is Autumn");
+            } else if (myMonth == 12 || myMonth == 1 || myMonth == 2) {
+                System.out.println("The season is Winter");
+            } else if (myMonth < 1 || myMonth > 12 ){
+                System.out.println("Entered number is out of range");
+            }
+        } else {
+            System.out.println("Sorry, couldn't understand you!");
+        }
     }
-
 }
