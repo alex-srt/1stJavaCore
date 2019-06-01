c28285f6777b98c3872a68c8581cdc2604f58ebe
lx5 <lexsirant@gmail.com>
2019-06-01 16:19:33 +0300

    46

--- src/Task_46.java
+++ src/Task_46.java
@@ -1,5 +1,12 @@
+/*Median number*/
+
 public class Task_46 {
     public static void main(String[] args) {
+        double a = 544;
+        double b = -324;
+        double c = 995.5544;
+        System.out.println("Median number of a b c numbers is " + (a+b+c)/3);
+
 
     }
 }
