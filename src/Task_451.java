325058786a940232bda375c995e46379505dacee
lx5 <lexsirant@gmail.com>
2019-11-03 15:37:39 +0200

    46

--- src/Task_451.java
+++ src/Task_451.java
@@ -1,18 +1,30 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task_451 {
     public static void main(String[] args) {
-        System.out.println("Enter an integer number: /n");
+        System.out.println("Enter Three digit number: /n");
         Scanner scan = new Scanner(System.in);
         if (scan.hasNextInt()) {
             int number = scan.nextInt();
-            String text = String.join(",", String.valueOf(number).split(""));
-            System.out.println("Entered numbers: " + text );
-            int num = Integer.parseInt(text[0]);
-            for
+//            String text = String.join(",", String.valueOf(number).split(""));
+//            System.out.println("Entered numbers: " + text );
 
-
-
+            String temp = Integer.toString(number);
+            int[] numArr = new int[temp.length()];
+            for (int i = 0; i < temp.length(); i++){
+                numArr[i] = temp.charAt(i) - '0';
+            }
+            System.out.println("Array: ");
+            System.out.println(Arrays.toString(numArr));
+            if (numArr[0] > numArr[2])
+            {
+                System.out.println(numArr[0] );
+            } else if (numArr[0] < numArr[2]){
+                System.out.println(numArr[2]);
+            } else if (numArr[0] == numArr[2]) {
+                System.out.println("=");
+            }
 
         } else {
             System.out.println("Not integer number entered!");
