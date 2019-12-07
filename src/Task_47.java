d0adf50fcb82585f1a58f0bb272a82244e582b09
AlexeySirant <lexsirant@gmail.com>
2019-12-07 20:45:56 +0200

    47

---
+++ src/Task_47.java
@@ -0,0 +1,38 @@
+/*
+* https://www.e-olymp.com/en/problems/125
+* Olympiad
+*
+*
+* The Olympiad started at h1 hours m1 minutes s1 seconds, and finished at the same calendar day at h2 hours m2 minutes s2 seconds.
+* What is the duration (hour min sec) of Olympiad?
+*
+*
+* Input
+* The first line contains the starting time,
+* the second line contains the finishing time in format hour min sec (0 ≤ h1 ≤ h2 ≤ 23, 0 ≤ m1, m2 ≤ 59, 0 ≤ s1, s2 ≤ 59.
+*
+* Output
+* Print the duration of the Olympiad in format hour min sec.
+* */
+
+public class Task_47 {
+    public static void main(String[] args) {
+
+        int h1 = 9;
+        int m1 = 30;
+        int s1 = 0;
+
+        int h2 = 12;
+        int m2 = 45;
+        int s2 = 30;
+
+
+
+
+
+
+
+    }
+
+
+}
